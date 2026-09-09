17bbc71e2911cf29f64281608541e9c8502c583e
115340225+cz-chan@users.noreply.github.com <115340225+cz-chan@users.noreply.github.com>
2026-09-09 13:14:44 +0200

    add works collection with schema and loader for content management

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,1 +1,22 @@
-export const collections = {};
+import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+
+const works = defineCollection({
+	loader: glob({
+		pattern: "**/*.{md,mdx}",
+		base: "./src/content/works",
+	}),
+	schema: z.object({
+		company: z.string().max(60),
+		role: z.string(),
+		description: z.string().min(110).max(160),
+		start: z.date(),
+		end: z.date().optional(),
+		where: z.string(),
+	}),
+});
+
+export const collections = {
+	works: works,
+};
