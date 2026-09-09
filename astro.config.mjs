10d09833395b97393486a703819d26829b922de6
115340225+cz-chan@users.noreply.github.com <115340225+cz-chan@users.noreply.github.com>
2026-09-09 09:55:48 +0200

    added satteri into config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import vercel from "@astrojs/vercel"
 import sitemap from '@astrojs/sitemap';
+import { satteri } from '@astrojs/markdown-satteri';
+
+import { hastExternalLinks } from '@plugins/markdown/hast-external-links.ts';
 
 // https://astro.build/config
 export default defineConfig( {
   site: "https://works.c12z.io",
   output: 'static',
+  markdown: {
+    processor: satteri( { hastPlugins: [ hastExternalLinks ] } ),
+  },
   integrations: [ mdx(), sitemap() ],
   adapter: vercel( {
     webAnalytics: {
