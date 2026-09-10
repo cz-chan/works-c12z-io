ca5e80dabd32ca14a549a88278b5d5b06f27b7ee
115340225+cz-chan@users.noreply.github.com <115340225+cz-chan@users.noreply.github.com>
2026-09-10 20:45:39 +0200

    refactor: remove unnecessary check for empty keys array in processKeywords function

--- src/utils/seo/process-keywords.ts
+++ src/utils/seo/process-keywords.ts
@@ -1,5 +1,5 @@
 export const processKeywords = (keys: string[]): string[] => {
-	if (keys.length === 0) return [];
+	// if (keys.length === 0) return []; // [].map() return by default an empty array
 
 	return Array.from(new Set(keys.map((kw) => kw.toLowerCase().trim())));
 	/**
