1631e863e8c89d0ce36c882139c5e523f8e5fa06
115340225+cz-chan@users.noreply.github.com <115340225+cz-chan@users.noreply.github.com>
2026-09-15 15:12:51 +0200

    added env schema for cloudflare turnstile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import vercel from "@astrojs/vercel"
@@ -12,6 +12,18 @@ import { hastExternalLinks } from '@plugins/markdown/hast-external-links.ts';
 export default defineConfig( {
   site: "https://works.c12z.io",
   output: 'static',
+  env: {
+    schema: {
+      CLOUDFLARE_TURNSTILE_PUBLIC_KEY: envField.string( {
+        context: 'client',
+        access: 'public',
+      } ),
+      CLOUDFLARE_TURNSTILE_SECRET_KEY: envField.string( {
+        context: 'server',
+        access: 'secret',
+      } ),
+    }
+  },
   markdown: {
     processor: satteri( { hastPlugins: [ hastExternalLinks ] } ),
   },
