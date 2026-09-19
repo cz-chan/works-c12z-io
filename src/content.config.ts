c78d4fdb5432a603bd520121a505c43c82d471ff
115340225+cz-chan@users.noreply.github.com <115340225+cz-chan@users.noreply.github.com>
2026-09-19 16:43:09 +0200

    fix: update works collection schema to use enums for stack and tools

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,6 +2,8 @@ import { glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
 import { z } from "astro/zod";
 
+import { ACTUAL_STACK, ACTUAL_TOOLS } from "@global/stack.ts";
+
 const works = defineCollection({
 	loader: glob({
 		pattern: "**/*.{md,mdx}",
@@ -14,7 +16,8 @@ const works = defineCollection({
 		start: z.date(),
 		end: z.date().optional(),
 		where: z.string(),
-		stack: z.array(z.string()).optional(),
+		stack: z.array(z.enum(ACTUAL_STACK)).optional(),
+		tools: z.array(z.enum(ACTUAL_TOOLS)).optional(),
 		url: z.string().startsWith("https://").optional(),
 		keywords: z.array(z.string()).optional(),
 	}),
