7b5cbd52e912ee779d1fe0d3e7a388324929d81a
115340225+cz-chan@users.noreply.github.com <115340225+cz-chan@users.noreply.github.com>
2026-09-19 17:14:19 +0200

    refactor: remove icon property from stack and tools definitions for simplification

--- src/global/stack.ts
+++ src/global/stack.ts
@@ -1,7 +1,6 @@
 interface StackToolItem {
 	name: string;
 	url: string;
-	icon: string;
 }
 
 export const ACTUAL_TOOLS = [
@@ -34,29 +33,25 @@ export const STACK: Record<ActualStackTypes, StackToolItem> = {
 	javascript: {
 		name: "JavaScript",
 		url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-		icon: "javascript",
 	},
 	typescript: {
 		name: "TypeScript",
 		url: "https://www.typescriptlang.org",
-		icon: "typescript",
 	},
-	astro: { name: "Astro", url: "https://astro.build", icon: "astro" },
-	react: { name: "React", url: "https://react.dev", icon: "react" },
-	nextjs: { name: "NextJS", url: "https://nextjs.org", icon: "nextjs" },
-	nodejs: { name: "NodeJS", url: "https://nodejs.org", icon: "nodejs" },
-	nestjs: { name: "NestJS", url: "https://nestjs.com", icon: "nestjs" },
-	express: { name: "Express", url: "https://expressjs.com", icon: "express" },
-	docker: { name: "Docker", url: "https://www.docker.com", icon: "docker" },
+	astro: { name: "Astro", url: "https://astro.build" },
+	react: { name: "React", url: "https://react.dev" },
+	nextjs: { name: "NextJS", url: "https://nextjs.org" },
+	nodejs: { name: "NodeJS", url: "https://nodejs.org" },
+	nestjs: { name: "NestJS", url: "https://nestjs.com" },
+	express: { name: "Express", url: "https://expressjs.com" },
+	docker: { name: "Docker", url: "https://www.docker.com" },
 	sql: {
 		name: "PostgreSQL",
 		url: "https://www.postgresql.org",
-		icon: "postgresql",
 	},
 	reactNative: {
 		name: "React Native",
 		url: "https://reactnative.dev",
-		icon: "react-native",
 	},
 };
 
@@ -64,36 +59,29 @@ export const TOOLS: Record<ActualToolsTypes, StackToolItem> = {
 	amplitude: {
 		name: "Amplitude",
 		url: "https://amplitude.com",
-		icon: "amplitude",
 	},
 	ranki: {
 		name: "Ranki",
 		url: "https://ranki.co",
-		icon: "ranki",
 	},
 	resend: {
 		name: "Resend",
 		url: "https://resend.com",
-		icon: "resend",
 	},
 	googleAnalytics: {
 		name: "Google Analytics",
 		url: "https://analytics.google.com",
-		icon: "ga4",
 	},
 	vwo: {
 		name: "VWO",
 		url: "https://vwo.com",
-		icon: "vwo",
 	},
 	clarity: {
 		name: "Clarity",
 		url: "https://clarity.microsoft.com",
-		icon: "clarity",
 	},
 	mixpanel: {
 		name: "Mixpanel",
 		url: "https://mixpanel.com",
-		icon: "mixpanel",
 	},
 };
